540097bf08e8f5f845f8abd859b76824030b69f7
Warley Franco <warleypsf@gmail.com>
2020-10-21 23:31:37 -0400

    Update post pages with post-api

--- pages/posts/[...post].tsx
+++ pages/posts/[...post].tsx
@@ -1,9 +1,36 @@
-import { useRouter } from 'next/router';
+import {useRouter} from 'next/router';
+import {getPostBySlug, slugWithoutExtension, getSlugs} from '../../lib/posts-api';
+import {GetStaticProps} from 'next';
 
 // TODO: Create single default post layout
 
-const Post = () => {
+export async function getStaticProps({params}) {
+  const allPaths = getSlugs().map(slug => slugWithoutExtension(slug));
+  return {
+    props: {
+      singlePost: {...getPostBySlug('test.md', ['title'])},
+      test: allPaths
+    }
+  }
+}
+
+export async function getStaticPaths() {
+
+  const allPaths = getSlugs().map(slug => {
+    return {
+      params: {post: [slugWithoutExtension(slug)]}
+    }
+  });
+
+  return {
+    paths: allPaths,
+    fallback: false
+  }
+}
+
+const Post = (props) => {
   const router = useRouter();
+  console.log(props);
   return (
     <h1>Slug: {router.query.post}</h1>
   )
--- pages/posts/index.tsx
+++ pages/posts/index.tsx
@@ -1,11 +1,32 @@
-const PostList = () => {
+import { fetchAllPosts } from '../../lib/posts-api';
+
+export async function getStaticProps() {
+
+  return {
+    props: { posts: [ ...fetchAllPosts() ] }
+  }
+}
+
+// TODO: Refactor post tiles (don't forget to split categories right @_@)
+
+const PostList = ({ posts }) => {
   return (
     <>
       <h1>Post List</h1>
+      <section>
+        {posts.map(post => (
+          <article key={post.title}>
+            <header>
+              <h2>{post.title}</h2>
+              <span>{post.date}</span>
+            </header>
+            <p>{post.description}</p>
+            <span>{posts.categories}</span>
+          </article>
+        ))}
+      </section>
     </>
   )
 }
-
-// TODO: Build post list
 
 export default PostList;
