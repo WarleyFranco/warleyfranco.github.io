ffa5368a46c938a11e08336d84deb8fbcd6c1680
Warley Franco <warleypsf@gmail.com>
2020-10-22 22:42:03 -0400

    Add post type + return normalized categories

--- lib/posts-api.ts
+++ lib/posts-api.ts
@@ -1,66 +1,64 @@
-import fs from 'fs';
-import path from 'path';
-import matter from 'gray-matter';
+import fs from 'fs'
+import path from 'path'
+import matter from 'gray-matter'
+import { Post } from '../types/post'
 
 // TODO: Create types for these method returns
 
-const postsDirectory = path.join(process.cwd(), '_posts');
+const postsDirectory = path.join(process.cwd(), '_posts')
 
 // Fetch all slugs in the target folder
 const getSlugs = () => {
-  return fs.readdirSync(postsDirectory);
+  return fs.readdirSync(postsDirectory)
 }
 
-const slugWithoutExtension = (slug) => slug.replace(/\.md$/g, '');
+const slugWithoutExtension = (slug): string => slug.replace(/\.md$/g, '')
 
-const getPostBySlug = (slug, fields = []) => {
-  const slugFullPath = path.join(postsDirectory, slug);
-  const slugContent = fs.readFileSync(slugFullPath);
-  const {content, data} = matter(slugContent);
+const getPostBySlug = (slug, fields = []): Post => {
+  const slugFullPath = path.join(postsDirectory, slug)
+  const slugContent = fs.readFileSync(slugFullPath)
+  const { content, data } = matter(slugContent)
 
   const item = {}
 
   fields.map(field => {
     if (field === 'slug') {
-      item[field] = slugWithoutExtension(slug);
+      item[field] = slugWithoutExtension(slug)
     }
 
     if (field === 'content') {
-      item[field] = content;
+      item[field] = content
+    }
+
+    if (field === 'categories') {
+      item[field] = data[field].split(',')
     }
 
     if (data[field]) {
       item[field] = data[field]
     }
-
   })
 
-  return item;
+  return item
 }
 
-const getAllPosts = () => {
-  const allSlugs = getSlugs();
-  const posts = [];
-  const fields = ['date', 'title', 'categories', 'description'];
+const getAllPosts = (): Post[] => {
+  const allSlugs = getSlugs()
+  const posts = []
+  const fields = ['date', 'title', 'categories', 'description']
 
   allSlugs.map(slug => {
-    const result = getPostBySlug(slug, fields);
-    result.date = new Date(result.date).getTime();
-    posts.push(result);
-  });
+    const result = getPostBySlug(slug, fields)
+    result.date = new Date(result.date).getTime()
+    posts.push(result)
+  })
 
   posts.sort((a, b) => {
     // Shortcut monstro do Iago!
-    return b.date - a.date;
+    return b.date - a.date
   })
 
-  return posts;
-
+  return posts
 }
 
-export {
-  getSlugs,
-  getAllPosts,
-  getPostBySlug,
-  slugWithoutExtension
-}
+export { getSlugs, getAllPosts, getPostBySlug, slugWithoutExtension }
