4a9aa60649adeb05ccbf5c9cc427dbbe5f8e028d
Warley Franco <warleypsf@gmail.com>
2020-10-22 22:47:06 -0400

    Prettier changes

--- pages/posts/[...post].tsx
+++ pages/posts/[...post].tsx
@@ -1,39 +1,35 @@
-import {useRouter} from 'next/router';
-import {getPostBySlug, slugWithoutExtension, getSlugs} from '../../lib/posts-api';
-import {GetStaticProps} from 'next';
+import { useRouter } from 'next/router'
+import { getPostBySlug, slugWithoutExtension, getSlugs } from '../../lib/posts-api'
+import { GetStaticProps } from 'next'
 
 // TODO: Create single default post layout
 
-export async function getStaticProps({params}) {
-  const allPaths = getSlugs().map(slug => slugWithoutExtension(slug));
+export async function getStaticProps({ params }) {
+  const allPaths = getSlugs().map((slug) => slugWithoutExtension(slug))
   return {
     props: {
-      singlePost: {...getPostBySlug('test.md', ['title'])},
-      test: allPaths
-    }
+      singlePost: { ...getPostBySlug('test.md', ['title', 'categories']) },
+    },
   }
 }
 
 export async function getStaticPaths() {
-
-  const allPaths = getSlugs().map(slug => {
+  const allPaths = getSlugs().map((slug) => {
     return {
-      params: {post: [slugWithoutExtension(slug)]}
+      params: { post: [slugWithoutExtension(slug)] },
     }
-  });
+  })
 
   return {
     paths: allPaths,
-    fallback: false
+    fallback: false,
   }
 }
 
 const Post = (props) => {
-  const router = useRouter();
-  console.log(props);
-  return (
-    <h1>Slug: {router.query.post}</h1>
-  )
+  const router = useRouter()
+  console.log(props)
+  return <h1>Slug: {router.query.post}</h1>
 }
 
-export default Post;
+export default Post
