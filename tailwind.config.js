736bcbb8e59a2f5963ffa86ab8f0973833203b3c
Warley Franco <warleypsf@gmail.com>
2021-04-24 12:51:56 -0400

    Add tailwindcss config

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    './src/pages/**/*.{ts, tsx}',
+    './src/components/**/*.{ts, tsx}'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
