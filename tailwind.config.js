5df384a8162deeb111d3b236d8c6f3e20631f0b1
Warley Franco <warleypsf@gmail.com>
2021-04-24 21:08:02 -0400

    Add tailwind theme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,26 @@
+const colors = require('tailwindcss/colors');
+
 module.exports = {
-  purge: [
-    './src/pages/**/*.{ts, tsx}',
-    './src/components/**/*.{ts, tsx}'
-  ],
+  purge: ['./src/pages/**/*.{ts, tsx}', './src/components/**/*.{ts, tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        black: colors.black,
+        blue: colors.indigo,
+        current: 'currentColor',
+        gray: colors.blueGray,
+        green: colors.emerald,
+        primary: colors.indigo,
+        red: colors.red,
+        transparent: 'transparent',
+        white: colors.white,
+        yellow: colors.amber,
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
