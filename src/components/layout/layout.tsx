697dcb08fa0ff47ae5d841452ba2d57d057c32c0
Warley Franco <warleypsf@gmail.com>
2021-04-25 00:46:21 -0400

    Update layout to use tailwind (wip)

--- src/components/layout/layout.tsx
+++ src/components/layout/layout.tsx
@@ -1,13 +1,18 @@
 import Header from './header';
+import Footer from './footer';
 
 const Layout = ({ children }) => {
   return (
-    <>
-      <Header />
-      { children }
-      <h2>Footer</h2>
-    </>
-  )
-}
+    <div className="min-h-screen flex flex-col flex-grow">
+      <div className="justify-self-start">
+        <Header />
+      </div>
+      <div className="flex-1">{children}</div>
+      <div className="justify-self-end">
+        <Footer />
+      </div>
+    </div>
+  );
+};
 
 export default Layout;
