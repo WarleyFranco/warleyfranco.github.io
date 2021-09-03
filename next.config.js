82fa80b98ede020bc881cbac5258c6bcaa6463c5
Warley Franco <warleypsf@gmail.com>
2021-09-02 23:08:59 -0400

    Update NextJS to 11.1.2;

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 module.exports = {
   i18n: {
-    locales: ['en', 'pt'],
-    defaultLocale: 'pt',
-  },
-  future: {
-    webpack5: true,
+    locales: ['en', 'pt-br'],
+    defaultLocale: 'en',
   },
 };
