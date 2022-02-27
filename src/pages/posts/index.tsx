fdbcbd0677fcbfbb08670bf4c1cd2997325a21f1
Warley Franco <warleypsf@gmail.com>
2022-02-26 21:35:36 -0500

    Formatting code (prettier)

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -1,24 +1,24 @@
-import React from 'react'
-import { getAllPostsByLocale } from '~/lib/posts-api'
-import { useContext } from 'react'
-import PostList from '~/components/post/post-list'
-import { CategoryContext } from '~/context'
-import { PageTitle } from '~/components/layout/typography'
+import React from 'react';
+import { getAllPostsByLocale } from '~/lib/posts-api';
+import { useContext } from 'react';
+import PostList from '~/components/post/post-list';
+import { CategoryContext } from '~/context';
+import { PageTitle } from '~/components/layout/typography';
 
 export async function getStaticProps({ locale }) {
   return {
     props: { posts: [...getAllPostsByLocale(locale)], locale },
-  }
+  };
 }
 
-const AllPosts = ({ posts }) => {
-  const { category } = useContext(CategoryContext)
+const AllPosts = ({ posts }): JSX.Element => {
+  const { category } = useContext(CategoryContext);
   return (
     <>
       <PageTitle>Post List: #{category}</PageTitle>
       <PostList posts={posts} />
     </>
-  )
-}
+  );
+};
 
-export default AllPosts
+export default AllPosts;
