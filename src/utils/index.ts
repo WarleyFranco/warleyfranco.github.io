4d4c5a7076e95ef6cf6697322b45e9819d08556e
Warley Franco <warleypsf@gmail.com>
2022-02-26 21:27:50 -0500

    Fix helper filename

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,2 +1,2 @@
-export { default as parseMarkdown } from './parseMarkdown'
-export { default as formatDate } from './formatDate'
+export { default as parseMarkdown } from './markdownToHtml';
+export { default as formatDate } from './formatDate';
