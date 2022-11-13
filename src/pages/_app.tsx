f24804dc663f58ab2fb31b189f792c064ed11c65
warleyfranco <warleypsf@gmail.com>
2022-11-12 22:13:39 -0500

    Add GA tag

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,21 @@ import Head from 'next/head';
 import { Layout } from '~/components/layout';
 import ContextProvider from '~/context/';
 import '~/styles/global.scss';
+import Script from 'next/script';
 
 function MainApplication({ Component, pageProps }) {
   return (
     <>
+      <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-J4L30DZK4K" />
+      <Script strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+  
+          gtag('config', 'G-J4L30DZK4K');
+        `}
+      </Script>
       <Head>
         <title>Warley Franco - Blog</title>
       </Head>
